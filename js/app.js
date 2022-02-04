e805f8583f94779c84bc0d18c2b3391b66c1aa3e
William Lee <williamlee.net@gmail.com>
2022-02-03 22:00:25 -0500

    Solve TO-DO LIST LAB

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
-console.log("sanity")
+// --==Cached element references==--
 
 const todoInput = document.getElementById('todo-input')
 const submitBtn = document.getElementById('submit-button')
 const todoList = document.getElementById('todo-list')
+const resetBtn = document.getElementById('reset-button')
+
+// --==Event Listeners==--
+
+// Functionality to add to to-dos   
+//element.addEventListener(event, function, useCapture)
+submitBtn.addEventListener('click', function (event) {
+  if (todoInput.value === "") return
+  const newTodo = document.createElement('li')
+  newTodo.textContent = todoInput.value
+  todoList.appendChild(newTodo)
+  todoInput.value = ''
+})
+
+// Functionality to remove individual to-dos
+todoList.addEventListener('click', function (event) {
+  const parentNode = event.target.parentNode
+  parentNode.removeChild(event.target)
+})
+
+// Functionality for Reset button to remove all to-dos
+resetBtn.addEventListener('click', function (event) {
+  todoList.innerHTML = ''
+  todoInput.value = ''
+})
